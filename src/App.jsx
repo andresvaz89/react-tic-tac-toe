2c8e62d24e577413b8395b0efe6ecc6987ba15be
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-29 20:32:49 +0100

    created board and initialiting state

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+
+const TURNS = {
+  x: 'x',
+  O: 'o'
+};
+
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
+
+function App() {
+  const [board, setBoard] = useState([
+    'x',
+    'x',
+    'x',
+    'o',
+    'o',
+    'o',
+    'x',
+    'o',
+    'x'
+  ]);
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
