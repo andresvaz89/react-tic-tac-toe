db23579d7b5eeed120cc58c128d7335f4097022e
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-30 14:09:14 +0100

    winner logic added

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`;
 
   const handleClick = () => {
-    updateBoard();
+    updateBoard(index);
   };
+
   return (
     <div onClick={handleClick} className={className}>
       {children}
@@ -19,14 +20,50 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
-  const updateBoard = () => {
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
