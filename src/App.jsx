38ffe86f478d22d84123c5f4a5330985106ebb0c
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-30 23:03:51 +0100

    added restart button and draw logic with message

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,10 @@ function App() {
     setWinner(null);
   };
 
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+  };
+
   const updateBoard = (index) => {
     if (board[index] || winner) return;
 
@@ -71,12 +75,15 @@ function App() {
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Restart the game</button>
       <section className="game">
         {board.map((_, index) => {
           return (
